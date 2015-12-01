aaba2ec6397712d2e5ac5be9d4b08573df4eeacd
Guillermo Iguaran <guilleiguaran@gmail.com>
2015-12-01 09:23:35 +0100

    First attemp of Contracts module

--- lib/contracts.ex
+++ lib/contracts.ex
@@ -1,2 +1,51 @@
 defmodule Contracts do
+  defmacro __using__(_opts) do
+    quote do
+      Module.register_attribute(__MODULE__, :requires, accumulate: true)
+      Module.register_attribute(__MODULE__, :ensures, accumulate: true)
+
+      @contracts %{}
+
+      import Kernel, except: [def: 2]
+      import Contracts, only: [def: 2]
+    end
+  end
+
+  defmacro def(definition, do: content) do
+    function_name = case definition do
+      {:when, _, [{name, _, _params} | _guards] } -> name
+      {name, _, _} -> name
+    end
+
+    contracts = Module.get_attribute(__CALLER__.module, :contracts)
+    precondition = contracts[function_name][:requires]
+
+    quote do
+      Contracts.__on_definition__(__ENV__, unquote(function_name))
+      #contracts = Module.get_attribute(__ENV__.module, :contracts)
+      #precondition = contracts[unquote(function_name)][:requires]
+      Kernel.def(unquote(definition)) do
+        unless unquote(precondition), do: raise "Contract not met: blame the client"
+        var!(result) = unquote(content)
+        var!(result)
+      end
+    end
+  end
+
+  def __on_definition__(env, function) do
+    mod = env.module
+
+    requires = Module.get_attribute(mod, :requires) |> List.first |> Code.string_to_quoted!
+    ensures = Module.get_attribute(mod, :ensures) |> List.first |> Code.string_to_quoted!
+    contract = %{requires: requires, ensures: ensures}
+
+    contracts = Module.get_attribute(mod, :contracts)
+
+    unless Map.has_key?(contracts, function) do
+      Module.put_attribute(mod, :contracts, Map.put(contracts, function, contract))
+    end
+
+    Module.delete_attribute(mod, :requires)
+    Module.delete_attribute(mod, :ensures)
+  end
 end
--- test/contracts_test.exs
+++ test/contracts_test.exs
@@ -6,28 +6,20 @@ defmodule ContractsTest do
 
     use Contracts
 
-    @requires not full?(tank) && tank.in_valve == :open && tank.out_valve == :closed
-    @ensures full?(result) && result.in_valve == :closed && result.out_valve == :closed
+    @requires "not full?(tank) && tank.in_valve == :open && tank.out_valve == :closed"
+    @ensures "full?(result) && result.in_valve == :closed && result.out_valve == :closed"
     def fill(tank) do
-
+      tank
     end
 
-    @requires tank.in_valve == :closed && tank.out_valve == :open
-    @ensures empty?(result) && result.in_valve == :closed && result.out_valve == :closed
+    @requires "tank.in_valve == :closed && tank.out_valve == :open"
+    @ensures "empty?(result) && result.in_valve == :closed && result.out_valve == :closed"
     def empty(tank) do
-
-    end
-
-    def full?(tank) do
-
-    end
-
-    def empty?(tank) do
-
+      tank
     end
   end
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "fill with bad args" do
+    #tank = %Tank{}
   end
 end
